6d6d39e9f6d35a053ff268ff572f583456a1b61b
DebasishSadangi2002 <101006162+DebasishSadangi2002@users.noreply.github.com>
2024-07-23 21:34:19 +0530

    home page added

--- myproject/urls.py
+++ myproject/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
     path('accounts/', include('accounts.urls')),  # Your accounts app URLs
     
         # Include gallery app URLs
